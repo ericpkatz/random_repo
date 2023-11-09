f0775472615cbfd741adb2b11126238b93ccfa54
ericpkatz <ericpkatz@gmail.com>
2023-11-09 11:28:43 -0500

    generate random numbers

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('hello world');
+function countSevens(arr){
+  let sevens = 0;
+  for(let i = 0; i < arr.length; i = i + 1){
+    if(arr[i] === 7){
+      sevens = sevens + 1;
+    }
+  }
+  return sevens;
+}
+
+function createRandoms(limit){
+  const numbers = [];
+  while(numbers.length < limit){
+    const num = Math.ceil(Math.random()*10);
+    numbers.push(num);
+  }
+  return numbers;
+}
+
+const numbers = createRandoms(1000000);
+
+/*
+let sevens = 0;
+for(let i = 0; i < numbers.length; i = i + 1){
+  if(numbers[i] === 7){
+    sevens = sevens + 1
+  }
+}
+
+console.log(sevens);
+*/
+//console.log(createRandoms(3));
+//console.log(countSevens([]));
+console.log(countSevens(numbers));
